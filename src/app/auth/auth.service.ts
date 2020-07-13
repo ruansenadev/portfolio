f6aad4051e29e44382d9805008b5ef182e6b1fbe
RuanSena <ruansenadev@gmail.com>
2020-07-13 16:44:40 -0300

    authenticated status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { Owner } from "./owner";
 
@@ -10,12 +11,24 @@ const apiAuth = environment.host + '/api/auth'
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
+  private isAuth: boolean
+  private authListener = new Subject<boolean>()
+
+  getStatus(): boolean {
+    return this.isAuth
+  }
+  getListener() {
+    return this.authListener.asObservable()
+  }
 
   login(email: string, password: string): void {
     const data = {email, password}
-    this.http.post<{account: Owner}>(apiAuth, data).subscribe((res) => {
-      console.log(res)
+    this.http.post<{message: string}>(apiAuth, data).subscribe((res) => {
+      this.isAuth = true;
+      this.authListener.next(true)
+      console.log(res.message)
+      this.router.navigate(['/'])
     })
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  constructor(private authService: AuthService) { }
+  isAuth: boolean
+  private authListen: Subscription
 
   ngOnInit(): void {
+    this.isAuth = this.authService.getStatus()
+    this.authListen = this.authService.getListener().subscribe((status) => this.isAuth = status)
   }
-
+  ngOnDestroy(): void {
+    this.authListen.unsubscribe()
+  }
 }
