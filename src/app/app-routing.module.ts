767df9ee65fc7b72afd6754614f33ad880499cce
RuanSena <ruansenadev@gmail.com>
2020-07-21 10:26:25 -0300

    contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { AuthGuard } from "./auth/auth.guard";
 import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }
 ]
