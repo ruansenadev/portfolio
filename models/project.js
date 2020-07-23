e0258c671f17a6041840aa96925441437bc735a2
RuanSena <ruansenadev@gmail.com>
2020-07-23 11:31:00 -0300

    projects form

--- models/project.js
+++ models/project.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose')
+const slugify = require('slug')
 const Schema = mongoose.Schema
 
 const projectSchema = new Schema({
   seq: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
-  status: { type: String, required: true, enum: ['Prototipagem', 'Desenvolvimento', 'Encerrado', 'Finalizado'] },
+  status: { type: String, required: true, enum: ['Protótipagem', 'Desenvolvimento', 'Encerrado', 'Finalizado'] },
   description: { type: String, required: true },
   thumbnailPath: { type: String },
   slug: { type: String, required: true },
   technologies: [String],
-  url: { type: String },
+  url: { type: String, required: true },
   homepage: { type: String },
   keywords: [String]
 })
 
 projectSchema.index({ reference: 1, homepage: 1 }, { unique: true })
+
+projectSchema.pre('validate', function (next) {
+  this.slug = slugify(this.name)
+  next()
+})
+projectSchema.pre('updateOne', function (next) {
+  this._update.slug = slugify(this._update.name)
+  next()
+})
 
 module.exports = mongoose.model('Project', projectSchema)
