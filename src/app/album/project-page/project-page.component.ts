b118af62a35389726089bb5da2ef52657829f67d
RuanSena <ruansenadev@gmail.com>
2020-07-25 18:45:08 -0300

    projects page

---
+++ src/app/album/project-page/project-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Project } from "../project";
+
+@Component({
+  selector: 'app-project-page',
+  templateUrl: './project-page.component.html',
+  styleUrls: ['./project-page.component.css']
+})
+export class ProjectPageComponent implements OnInit {
+  constructor(private route: ActivatedRoute) { }
+  project: Project
+  ngOnInit(): void {
+    this.project = this.route.snapshot.data['project']
+  }
+}
