2b32a2609a0a395c43954a8b7a84793818eab106
RuanSena <ruansenadev@gmail.com>
2020-07-26 17:37:18 -0300

    error message

---
+++ src/app/messages/error.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from "rxjs/operators";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MessageComponent } from "./message/message.component";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private messageBar: MatSnackBar) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler) {
+    return next.handle(request).pipe(
+      catchError((res: HttpErrorResponse) => {
+        this.messageBar.openFromComponent(MessageComponent, {
+          data: { message: res.error.message, action: 'Ok' },
+          horizontalPosition: 'left'
+        })
+        return throwError(res)
+      })
+    )
+  }
+}
