7ddc410ab7a34863f57cbe100d71ee74f3c1c6f0
RuanSena <ruansenadev@gmail.com>
2020-07-28 10:45:01 -0300

    rename

--- src/app/home/home-contact/home-contact.component.spec.ts
+++ src/app/home/home-contact/home-contact.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactComponent } from './contact.component';
+import { HomeContactComponent } from './home-contact.component';
 
 describe('ContactComponent', () => {
-  let component: ContactComponent;
-  let fixture: ComponentFixture<ContactComponent>;
+  let component: HomeContactComponent;
+  let fixture: ComponentFixture<HomeContactComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ HomeContactComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactComponent);
+    fixture = TestBed.createComponent(HomeContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
