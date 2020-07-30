6fb8e9f6e826ca0e8a93a371eda5add7e24d3dfb
RuanSena <ruansenadev@gmail.com>
2020-07-30 12:18:47 -0300

    admin navigation

--- src/app/dispatcher/dispatcher.component.ts
+++ src/app/dispatcher/dispatcher.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AdminService } from "../admin/admin.service";
 import { Admin } from "../admin/admin";
+import { AuthService } from "../auth/auth.service";
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dispatcher',
@@ -11,8 +13,12 @@ import { Admin } from "../admin/admin";
   styleUrls: ['./dispatcher.component.css']
 })
 export class DispatcherComponent implements OnInit {
-  constructor(private breakpointObserver: BreakpointObserver, private adminService: AdminService) { }
+  constructor(private breakpointObserver: BreakpointObserver, private adminService: AdminService, private authService: AuthService) { }
+  @ViewChild('drawer') nav: MatSidenav
+  @ViewChild('_drawer') navAdmin: MatSidenav
   account: Admin
+  isAuth: boolean
+  authListener: Subscription
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -20,5 +26,28 @@ export class DispatcherComponent implements OnInit {
     );
   ngOnInit(): void {
     this.adminService.getAdmin().subscribe((account) => this.account = account)
+    this.isAuth = this.authService.getStatus()
+    this.authListener = this.authService.getListener().subscribe((status) => this.isAuth = status)
+  }
+  onToggle(nav: string): void {
+    (this[nav] as MatSidenav).toggle()
+  }
+  onNav(nav: string): void {
+    this.isHandset$.subscribe((matches) => {
+      if (matches) {
+        (this[nav] as MatSidenav).close()
+      }
+    })
+  }
+  onLogout(e: Event): void {
+    e.preventDefault()
+    this.isHandset$.subscribe((matches) => {
+      if (matches) {
+        this.navAdmin.close()
+        this.navAdmin._closedStream.subscribe(() => this.authService.logout())
+      } else {
+        this.authService.logout()
+      }
+    })
   }
 }
