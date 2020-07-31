634268f9978dcae0d7e582782019f0e06fdae6ee
RuanSena <ruansenadev@gmail.com>
2020-07-31 17:50:58 -0300

    errors feedback

--- routes/projects.js
+++ routes/projects.js
@@ -18,25 +18,25 @@ const projectFormOptions = {
 }
 const allowedTypes = ["image/png", "image/jpeg", "image/svg+xml", "image/webp"]
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   Project.find({})
     .lean()
     .sort('-seq')
     .exec((err, projects) => {
-      if (err) { return next(err) }
+      if (err) { return res.status(502).json({ message: 'Falha ao buscar projetos' }) }
       res.json(projects)
     })
 })
-router.get('/:seq', function (req, res, next) {
+router.get('/:seq', function (req, res) {
   Project.findOne({ seq: +req.params.seq })
     .lean()
     .exec((err, project) => {
-      if (err) { return next(err) }
-      if (!project) { return res.status(404).json({ message: 'Projeto inexistente.' }) }
+      if (err) { return res.status(502).json({ message: 'Falha ao buscar' }) }
+      if (!project) { return res.status(404).json({ message: 'Projeto inexistente' }) }
       res.json(project)
     })
 })
-router.post('/', Auth, function (req, res, next) {
+router.post('/', Auth, function (req, res) {
   const form = new IncomingForm(projectFormOptions)
   const dateUpload = moment().format('DD-MM-YYYY-hh-mm-ss')
   form.on("fileBegin", function (filename, file) {
@@ -46,13 +46,13 @@ router.post('/', Auth, function (req, res, next) {
     if (part.mime) {
       if (!allowedTypes.includes(part.mime)) {
         req.destroy()
-        return res.status(406).json({ message: 'Mime-type inválido.' })
+        return res.status(415).json({ message: 'Mime-type inválido' })
       }
     }
     form.handlePart(part)
   }
   form.parse(req, function (err, fields, files) {
-    if (err) { return next(err) }
+    if (err) { return res.status(400).json({ message: 'Formulário inválido' }) }
     let project = new Project({
       name: fields.name,
       status: fields.status,
@@ -64,18 +64,25 @@ router.post('/', Auth, function (req, res, next) {
     if (files.thumbnail) project.thumbnailPath = `${req.protocol}://${req.get('host')}/images/album/${dateUpload}-${files.thumbnail.name}`;
     if (fields.overview) project.overview = fields.overview;
     if (fields.homepage) project.homepage = fields.homepage;
-    Counter.findByIdAndUpdate(counterId, { $inc: { seq: 1 } }, { new: true })
-      .then((counter) => {
+    Counter.findById(counterId)
+      .then((projectCount) => {
         project.seq = counter.seq
-        project.save((err, projectSaved) => {
-          if (err) { return next(err) }
-          res.json({ message: 'Projeto adicionado!', project: projectSaved })
-        })
-      })
-      .catch(next)
+        project.save()
+          .then((projectSaved) => {
+            projectCount.updateOne({ seq: projectCount.seq + 1 })
+              .then(() => {
+                res.status(200).json({ message: 'Projeto adicionado!', project: projectSaved })
+              }).catch(() => {
+                projectSaved.deleteOne((err) => {
+                  if (err) { return res.status(502).json({ message: 'Falha, delete o projeto da DB!' }) }
+                  res.status(502).json({ message: 'Falha ao salvar' })
+                })
+              })
+          })
+      }).catch(() => { return res.status(502).json({ message: 'Falha ao salvar' }) })
   })
 })
-router.put('/:id', Auth, function (req, res, next) {
+router.put('/:id', Auth, function (req, res) {
   const form = new IncomingForm(projectFormOptions)
   const dateUpload = moment().format('DD-MM-YYYY-hh-mm-ss')
   form.on("fileBegin", function (filename, file) {
@@ -85,13 +92,13 @@ router.put('/:id', Auth, function (req, res, next) {
     if (part.mime) {
       if (!allowedTypes.includes(part.mime)) {
         req.destroy()
-        return res.status(406).json({ message: 'Mime-type inválido.' })
+        return res.status(415).json({ message: 'Mime-type inválido' })
       }
     }
     form.handlePart(part)
   }
   form.parse(req, function (err, fields, files) {
-    if (err) { next(err) }
+    if (err) { return res.status(400).json({ message: 'Formulário inválido' }) }
     let project = new Project({
       _id: fields._id,
       seq: +fields.seq,
@@ -107,9 +114,8 @@ router.put('/:id', Auth, function (req, res, next) {
     if (fields.overview) project.overview = fields.overview;
     if (fields.homepage) project.homepage = fields.homepage;
     Project.updateOne({ _id: req.params.id }, project, (err, result) => {
-      if (err) { return next(err) }
-      if (!result.n) { return res.status(400).json({ message: 'Falha ao atualizar.' }) }
-      res.json({ message: 'Projeto atualizado!' })
+      if (err || !result.n) { return res.status(502).json({ message: 'Falha ao atualizar' }) }
+      res.status(200).json({ message: 'Projeto atualizado!' })
     })
   })
 })
@@ -122,14 +128,14 @@ router.delete('/:id', Auth, function (req, res) {
       Counter.findById(counterId, cb)
     }
   }, (err, results) => {
-    if (err) { return res.status(400).json({ message: 'Falha ao deletar.' }) }
+    if (err) { return res.status(400).json({ message: 'Falha ao deletar' }) }
     if (results.last.seq === results.projectDeleted.seq) {
       Counter.updateOne({ _id: counterId }, { $inc: { seq: -1 } }, (err) => {
-        if (err) { return res.json({ message: `Projeto ${results.projectDeleted.seq} deletado.` }) }
-        res.json({ message: `Último projeto deletado.` })
+        if (err) { return res.status(502).json({ message: `Falha ao atualizar sequência ${results.projectDeleted.seq}` }) }
+        res.status(200).json({ message: `Último projeto deletado.` })
       })
     } else {
-      res.json({ message: `Projeto ${results.projectDeleted.seq} deletado.` })
+      res.status(200).json({ message: `Projeto ${results.projectDeleted.seq} deletado.` })
     }
   })
 })
