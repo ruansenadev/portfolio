f8305dd1b3eca05d28e37aace6791be613273dd8
RuanSena <ruansenadev@gmail.com>
2020-08-04 15:49:24 -0300

    profile form state

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -12,23 +12,24 @@ import { AdminService } from "../admin.service";
 export class DashboardComponent implements OnInit {
   constructor(private breakpointObserver: BreakpointObserver, private adminService: AdminService) {}
   @Output() account: Admin
+  @Output() profileRead: boolean = true
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Perfil', order: '1', cols: 1, rows: 2 },
+          { title: '2', order: '2', cols: 1, rows: 1 },
+          { title: '3', order: '3', cols: 1, rows: 1 },
+          { title: '4', order: '4', cols: 1, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Perfil', order: '1', cols: 2, rows: 1 },
+        { title: '2', order: '2', cols: 1, rows: 1 },
+        { title: '3', order: '3', cols: 1, rows: 2 },
+        { title: '4', order: '4', cols: 1, rows: 1 }
       ];
     })
   );
