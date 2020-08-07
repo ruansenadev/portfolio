5589f95a43c5179a6f6076c8e62792703823ab96
RuanSena <ruansenadev@gmail.com>
2020-08-07 19:13:01 -0300

    professional data form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ENTER, COMMA } from "@angular/cdk/keycodes";
 import { AdminRoutingModule } from "./admin-routing.module";
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -14,16 +15,19 @@ import { MatRadioModule } from '@angular/material/radio';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from "@angular/material/core";
+import { MatChipsModule, MAT_CHIPS_DEFAULT_OPTIONS } from "@angular/material/chips";
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { AccountFormComponent } from './account-form/account-form.component';
+import { ProfessionalFormComponent } from './professional-form/professional-form.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProfileFormComponent,
-    AccountFormComponent
+    AccountFormComponent,
+    ProfessionalFormComponent
   ],
   imports: [
     CommonModule,
@@ -39,7 +43,11 @@ import { AccountFormComponent } from './account-form/account-form.component';
     MatRadioModule,
     ReactiveFormsModule,
     MatNativeDateModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatChipsModule
+  ],
+  providers: [
+    { provide: MAT_CHIPS_DEFAULT_OPTIONS, useValue: { separatorKeyCodes: [ENTER, COMMA] } }
   ]
 })
 export class AdminModule { }
