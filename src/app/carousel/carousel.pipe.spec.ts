d8221b00f0dee2f375464949123add92ab264e8e
RuanSena <ruansenadev@gmail.com>
2020-08-11 16:52:07 -0300

    input transform

---
+++ src/app/carousel/carousel.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CarouselPipe } from './carousel.pipe';
+
+describe('CarouselPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CarouselPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
