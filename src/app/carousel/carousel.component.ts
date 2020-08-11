94cb1e6f3dfd37f28bc933a004003453d4b171b8
RuanSena <ruansenadev@gmail.com>
2020-08-11 11:37:13 -0300

    items data input

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 export interface Item {
   title: string;
-  no: number;
+  type: string;
   img: string;
   desc: string;
+  uri: string;
 }
 
 @Component({
@@ -12,15 +13,14 @@ export interface Item {
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
   constructor() { }
-  items: Item[] = [
-    {title: 'Item 1', no: 1, img: 'https://www.stevensegallery.com/360/170', desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam tenetur explicabo unde eos! Ducimus, reprehenderit exercitationem? Ipsa dolorum repellendus saepe beatae eaque, veritatis quibusdam dicta maiores non, eos nam.'},
-    {title: 'Item 2', no: 2, img: 'https://www.stevensegallery.com/360/320', desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam tenetur explicabo unde eos! Ducimus, reprehenderit exercitationem? Ipsa dolorum repellendus saepe beatae eaque, veritatis quibusdam dicta maiores non, eos nam.'},
-    {title: 'Item 3', no: 3, img: 'https://www.stevensegallery.com/420/280', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis nemo vitae laudantium, exercitationem ullam provident sapiente sit repellendus consequatur veniam, cumque tempora, architecto tempore! Esse, deserunt? Voluptatum vel id quos!'},
-    {title: 'Item 4', no: 4, img: 'https://www.stevensegallery.com/300/210', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit illum neque suscipit animi nesciunt blanditiis, incidunt ad nostrum perspiciatis. Ex autem velit nostrum quibusdam unde quas obcaecati sapiente quidem voluptate!'},
+  @Input() title: string = ''
+  @Input() items: Item[] = []
+  cards: Item[] = [
+    {title: 'Item 1', type: 'batata', img: 'https://www.stevensegallery.com/360/170', desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam tenetur explicabo unde eos! Ducimus, reprehenderit exercitationem? Ipsa dolorum repellendus saepe beatae eaque, veritatis quibusdam dicta maiores non, eos nam.', uri: '/'},
+    {title: 'Item 2', type: 'batata', img: 'https://www.stevensegallery.com/360/320', desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nam tenetur explicabo unde eos! Ducimus, reprehenderit exercitationem? Ipsa dolorum repellendus saepe beatae eaque, veritatis quibusdam dicta maiores non, eos nam.', uri: '/'},
+    {title: 'Item 3', type: 'batata', img: 'https://www.stevensegallery.com/420/280', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis nemo vitae laudantium, exercitationem ullam provident sapiente sit repellendus consequatur veniam, cumque tempora, architecto tempore! Esse, deserunt? Voluptatum vel id quos!', uri: '/'},
+    {title: 'Item 4', type: 'batata', img: 'https://www.stevensegallery.com/300/210', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit illum neque suscipit animi nesciunt blanditiis, incidunt ad nostrum perspiciatis. Ex autem velit nostrum quibusdam unde quas obcaecati sapiente quidem voluptate!', uri: '/'},
   ]
-  ngOnInit(): void {
-  }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Project } from "../album/project";
+import { ProjectsService } from "../album/projects.service";
+import { Post } from "../blog/post";
+import { PostsService } from "../blog/posts.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  projects: Project[]
+  posts: Post[]
+  private projectsListener: Subscription
+  private postsListener: Subscription
+  constructor(private projectsService: ProjectsService, private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.projectsService.populateProjects()
+    this.projectsListener = this.projectsService.getStream().subscribe((projects) => this.projects = projects)
+    this.postsService.populatePosts()
+    this.postsListener = this.postsService.getStream().subscribe((sub) => this.posts = sub.posts)
   }
-
+  ngOnDestroy(): void {
+    this.postsListener.unsubscribe()
+    this.projectsListener.unsubscribe()
+  }
 }
