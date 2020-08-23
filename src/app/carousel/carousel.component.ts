606d3c410aaed6d40c0f9c1b5fd4d311a04e4687
RuanSena <ruansenadev@gmail.com>
2020-08-23 17:43:39 -0300

    improvements

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,7 +1,5 @@
-import { Component, Input, OnChanges, SimpleChange, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { ShareService } from "../share/share.service";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { map } from 'rxjs/operators';
 
 export interface Item {
   title: string;
@@ -15,12 +13,12 @@ export interface Item {
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnChanges {
-  constructor(private shareService: ShareService, private breakpointObserver: BreakpointObserver) { }
+export class CarouselComponent {
+  constructor(private shareService: ShareService) { }
   @ViewChild('box') box: ElementRef
+  private tolerance = 32
   private pressPosition: number = null
-  private width: number
-  private view: number
+  private outBoxSlice: number
   private transformState: number = 0
   private isMoving: boolean = false
   @Input() title: string = ''
@@ -28,33 +26,35 @@ export class CarouselComponent implements OnChanges {
   onShare(uri: string) {
     this.shareService.openSheet(uri)
   }
-  ngOnChanges(changes: { [key: string]: SimpleChange }) {
-    if (changes['items']) {
-      if (this.items) {
-        // observe for gesture events availability
-      }
-    }
+  updateTransformState(): void {
+    const state = (this.box.nativeElement as HTMLDivElement).style.getPropertyValue('transform')
+    this.transformState = +state.slice(state.indexOf('(') + 1, state.indexOf('px'))
   }
   onPress(e: PointerEvent | MouseEvent): void {
     const box = (this.box.nativeElement as HTMLDivElement)
-    this.width = box.scrollWidth
-    this.view = box.clientWidth
+    this.outBoxSlice = box.scrollWidth - box.clientWidth
     this.pressPosition = e.pageX
     this.isMoving = true
-    const state = box.style.getPropertyValue('transform')
-    this.transformState = +state.slice(state.indexOf('(') + 1, state.indexOf('px'))
+    this.updateTransformState()
   }
   onMove(e: PointerEvent | MouseEvent): void {
     if (this.isMoving) {
       const currentPosition = e.pageX
       const X = this.transformState + (currentPosition - this.pressPosition)
-      const outBoxSlice = this.width - this.view
+
       // case actual position plus difference (less or more) is going to move backward from start or forward from end
-      if (X > 0 || Math.abs(X) > outBoxSlice ) { return }
+      if (X > (0 + this.tolerance) || Math.abs(X) > (this.outBoxSlice + this.tolerance)) { return }
       this.box.nativeElement.style.transform = `translateX(${X}px)`
     }
   }
   onRelease(e: PointerEvent | MouseEvent): void {
     this.isMoving = false
+    // restores inner state
+    this.updateTransformState()
+    if(this.transformState > 0) {
+      this.box.nativeElement.style.transform = `translateX(${0}px)`
+    } else if (Math.abs(this.transformState) > this.outBoxSlice) {
+      this.box.nativeElement.style.transform = `translateX(${-(this.outBoxSlice)}px)`
+    }
   }
 }
