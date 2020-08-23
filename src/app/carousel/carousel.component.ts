58a1e1715a657cb106f952ecbb4fb3a7ae59e8a8
RuanSena <ruansenadev@gmail.com>
2020-08-23 16:57:50 -0300

    prevent void

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -19,6 +19,8 @@ export class CarouselComponent implements OnChanges {
   constructor(private shareService: ShareService, private breakpointObserver: BreakpointObserver) { }
   @ViewChild('box') box: ElementRef
   private pressPosition: number = null
+  private width: number
+  private view: number
   private transformState: number = 0
   private isMoving: boolean = false
   @Input() title: string = ''
@@ -34,16 +36,22 @@ export class CarouselComponent implements OnChanges {
     }
   }
   onPress(e: PointerEvent | MouseEvent): void {
+    const box = (this.box.nativeElement as HTMLDivElement)
+    this.width = box.scrollWidth
+    this.view = box.clientWidth
     this.pressPosition = e.pageX
     this.isMoving = true
-    const state = (this.box.nativeElement as HTMLDivElement).style.getPropertyValue('transform')
+    const state = box.style.getPropertyValue('transform')
     this.transformState = +state.slice(state.indexOf('(') + 1, state.indexOf('px'))
   }
   onMove(e: PointerEvent | MouseEvent): void {
     if (this.isMoving) {
       const currentPosition = e.pageX
-      const diff = currentPosition - this.pressPosition
-      this.box.nativeElement.style.transform = `translateX(${this.transformState + diff}px)`
+      const X = this.transformState + (currentPosition - this.pressPosition)
+      const outBoxSlice = this.width - this.view
+      // case actual position plus difference (less or more) is going to move backward from start or forward from end
+      if (X > 0 || Math.abs(X) > outBoxSlice ) { return }
+      this.box.nativeElement.style.transform = `translateX(${X}px)`
     }
   }
   onRelease(e: PointerEvent | MouseEvent): void {
