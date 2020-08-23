9117182a233165590ba0b74b4903d9273fc238bc
RuanSena <ruansenadev@gmail.com>
2020-08-23 14:53:30 -0300

    translate slide logic

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange, ViewChild, ElementRef } from '@angular/core';
 import { ShareService } from "../share/share.service";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { map } from 'rxjs/operators';
 
 export interface Item {
   title: string;
@@ -13,11 +15,38 @@ export interface Item {
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
-  constructor(private shareService: ShareService) { }
+export class CarouselComponent implements OnChanges {
+  constructor(private shareService: ShareService, private breakpointObserver: BreakpointObserver) { }
+  @ViewChild('box') box: ElementRef
+  private pressPosition: number = null
+  private transformState: number = 0
+  private isMoving: boolean = false
   @Input() title: string = ''
   @Input() items: Item[] = []
   onShare(uri: string) {
     this.shareService.openSheet(uri)
+  }
+  ngOnChanges(changes: { [key: string]: SimpleChange }) {
+    if (changes['items']) {
+      if (this.items) {
+        // observe for gesture events availability
+      }
+    }
+  }
+  onPress(e: MouseEvent): void {
+    this.pressPosition = e.pageX
+    this.isMoving = true
+    const state = (this.box.nativeElement as HTMLDivElement).style.getPropertyValue('transform')
+    this.transformState = +state.slice(state.indexOf('(') + 1, state.indexOf('px'))
+  }
+  onMove(e: MouseEvent): void {
+    if (this.isMoving) {
+      const currentPosition = e.pageX
+      const diff = currentPosition - this.pressPosition
+      this.box.nativeElement.style.transform = `translateX(${this.transformState + diff}px)`
+    }
+  }
+  onRelease(e: MouseEvent): void {
+    this.isMoving = false
   }
 }
