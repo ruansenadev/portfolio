2ae49eef2389c2fe7e1bbfacc267aadbf960274a
RuanSena <ruansenadev@gmail.com>
2020-08-31 16:20:29 -0300

    archives c

---
+++ src/app/blog/blog-archives/blog-archives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blog-archives',
+  templateUrl: './blog-archives.component.html',
+  styleUrls: ['./blog-archives.component.css']
+})
+export class BlogArchivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
