564500b835066c383f47163fd3c933517771b792
RuanSena <ruansenadev@gmail.com>
2020-09-04 15:02:38 -0300

    router options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,11 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    paramsInheritanceStrategy: 'always',
+    relativeLinkResolution: 'corrected'
+  })],
   exports: [RouterModule]
 })
 
