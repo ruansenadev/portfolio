96c5a67d9bf8c49eb0b59b3620c0e19916e14135
RuanSena <ruansenadev@gmail.com>
2020-09-22 12:30:11 -0300

    package reduce

--- server/models/counter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const counterSchema = new Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, required: true }
-})
-
-module.exports = mongoose.model('Counter', counterSchema)
