0c5c0a274d42c056734b310cb29968bc2ba1736f
Ruan Sena <ruansenadev@gmail.com>
2020-11-11 12:36:32 -0300

    image filename transform

---
+++ src/app/util/image-storage.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageStorageService {
+  fileNameRegex = /(^[\w-]+)\.(?:png$|jpg$|jpeg$|svg$|webp$)/;
+  constructor(private http: HttpClient) { }
+  allowFileName(fileName: string): string {
+    const dateString = new Date().toLocaleString().replace(/\//g, '_').replace(/[\s:]/g, '-');
+    const slices = fileName.split('.');
+
+    if (this.fileNameRegex.test(fileName) && this.fileNameRegex.exec(fileName)[0] === fileName) {
+      return `${slices[0]}_${dateString}.${slices[1]}`;
+    }
+    return slices.reduce((safeFileName, slice, i) => {
+      if (slices.length - 1 === i) {
+        return `${safeFileName}${dateString}.${slice}`;
+      }
+      return safeFileName += /[\w-]/g.test(slice) && slice.match(/[\w-]/g).join('') + '_';
+    }, '');
+  }
+  validateFile(file: File): File {
+    const fileName = this.allowFileName(file.name);
+    const blob = file.slice(0, file.size, file.type);
+    return new File([blob], fileName, { type: file.type });
+  }
+}
+
+
